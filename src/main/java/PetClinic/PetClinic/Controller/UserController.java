7038229643e22d71735c64ef4a8258358e57871f
Muhamad-Rezky-Pratama <muhamad.rezky@mhs.itenas.ac.id>
2025-05-19 13:38:12 +0700

    .

---
+++ src/main/java/PetClinic/PetClinic/Controller/UserController.java
@@ -0,0 +1,46 @@
+package PetClinic.PetClinic.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import PetClinic.PetClinic.Model.User;
+import PetClinic.PetClinic.Repository.UserRepository;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegister(@ModelAttribute("user") User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        if (user.getRole() == null) user.setRole("USER");
+        userRepo.save(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/")
+    public String homePage() {
+        return "home";
+    }
+}
---
+++ src/main/java/PetClinic/PetClinic/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package PetClinic.PetClinic.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import PetClinic.PetClinic.Model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
